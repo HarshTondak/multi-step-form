aee7965364bc35390d00ff9c5407eca103cb346c
Harsh Tondak <harshtondak007@gmail.com>
2024-02-26 00:53:28 +0530

    some fixes

--- app/(landing-page)/eighth-section.tsx
+++ app/(landing-page)/eighth-section.tsx
@@ -6,7 +6,7 @@ const GetStartedFree = () => {
     <>
       <div className="py-20 xl:py-24 flex justify-center items-center flex-col border-b">
         <div className="text-4xl xl:text-5xl font-medium  text-center">
-          Got Questions? Let's Connect
+          Got Questions? Let&apos;s Connect
         </div>
         <div className="text-sky-500 my-4 flex items-center hover:underline hover:cursor-pointer">
           Request a demo <PiArrowRight className="ml-3 text-sm " />
