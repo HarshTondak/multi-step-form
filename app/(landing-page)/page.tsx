28439f42173a4122f92ca36af3020db6a1887612
Harsh Tondak <harshtondak007@gmail.com>
2024-02-26 01:04:48 +0530

    updates done

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar/navbar";
 import HeroSection from "./hero-section";
 import SecondSection from "./second-section";
 import ThirdSection from "./third-section";
-import GetStartedFree from "./eighth-section";
+import GetStartedFree from "./fourth-section";
 import Footer from "@/components/footer";
 import Form from "./form";
 
